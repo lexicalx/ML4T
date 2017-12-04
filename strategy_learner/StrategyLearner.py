530a25d04087e20eac3b2f21ac95bf6d92ca9c9f
vinay <vsrinath6@gatech.edu>
2017-12-04 09:50:00 -0500

    sL #2

--- strategy_learner/StrategyLearner.py
+++ strategy_learner/StrategyLearner.py
@@ -33,25 +33,23 @@ class StrategyLearner(object):
         prices = ut.get_data([symbol], lookback_dates)
         prices = prices.ix[:, [symbol]]
 
-        ## Get indicators with 14 days lookback period from sd
-        priceOverSMARatio = priceOverSMA(symbol,prices)
-        bbpValue = bbp(symbol,prices)
-        rsiValue = rsi(symbol,prices)
+        priceOverSMAValues = priceOverSMA(symbol,prices)
+        bbpValues = bbp(symbol,prices)
+        rsiValues = rsi(symbol,prices)
 
-        ## Get prices starting from sd
-        prices = prices.ix[sd:]
+        self.prices = prices.ix[sd:]
+        self.symbol = symbol
 
-        # discretize state
-        priceOverSMARatio = priceOverSMARatio.ix[sd:]
-        priceOverSMARatio[symbol] = pd.qcut(priceOverSMARatio[symbol].values, 10).codes
+        priceOverSMAValues = priceOverSMAValues.ix[sd:]
+        priceOverSMAValues[symbol] = pd.qcut(priceOverSMAValues[symbol].values, 10).codes
 
-        bbpValue = bbpValue.ix[sd:]
-        bbpValue[symbol] = pd.qcut(bbpValue[symbol].values, 10).codes
+        bbpValues = bbpValues.ix[sd:]
+        bbpValues[symbol] = pd.qcut(bbpValues[symbol].values, 10).codes
 
-        rsiValue = rsiValue.ix[sd:]
-        rsiValue[symbol] = pd.qcut(rsiValue[symbol].values, 10).codes
+        rsiValues = rsiValues.ix[sd:]
+        rsiValues[symbol] = pd.qcut(rsiValues[symbol].values, 10).codes
 
-        states = (priceOverSMARatio * 100) + (bbpValue * 10) + rsiValue * 1
+        states = (priceOverSMAValues * 100) + (bbpValues * 10) + rsiValues * 1
 
         converged = False
         count = 0
@@ -67,14 +65,16 @@ class StrategyLearner(object):
             for day in range(1, num_days):
                 # implement action
                 net_position = action
-                if net_position == 1:
-                    reward = 0
-                elif net_position == 2:
-                    # get daily return percetage
-                    reward = prices.ix[day, symbol] / prices.ix[day - 1, symbol] - 1
-                elif net_position == 0:
-                    # get daily return percetage
-                    reward = -1 * (prices.ix[day, symbol] / prices.ix[day - 1, symbol] - 1)
+                # if net_position == 1:
+                #     reward = 0
+                # elif net_position == 2:
+                #     # get daily return percetage
+                #     reward = (prices.ix[day, symbol] / prices.ix[day - 1, symbol] - 1) - self.impact
+                # elif net_position == 0:
+                #     # get daily return percetage
+                #     reward = -1 * (prices.ix[day, symbol] / prices.ix[day - 1, symbol] - 1 - self.impact)
+                # total_reward += reward
+                reward = self.getReward(net_position,day)
                 total_reward += reward
                 state = states.ix[day, symbol]
                 action = self.learner.query(state, reward)
@@ -95,38 +95,124 @@ class StrategyLearner(object):
 
         lookback_dates = pd.date_range(sd - dt.timedelta(days=30), ed)
 
-        price = ut.get_data([symbol], lookback_dates)
-        price = price.ix[:, [symbol]]
+        prices = ut.get_data([symbol], lookback_dates)
+        prices = prices.ix[:, [symbol]]
 
-        smaR = priceOverSMA(symbol,price)
-        bbpV = bbp(symbol,price)
-        rsiV = rsi(symbol,price)
+        priceOverSMAValues = priceOverSMA(symbol,prices)
+        bbpValues = bbp(symbol,prices)
+        rsiValues = rsi(symbol,prices)
 
-        price = price.ix[sd:]
+        self.prices = prices.ix[sd:]
+        self.symbol = symbol
+        priceOverSMAValues = priceOverSMAValues.ix[sd:]
+        priceOverSMAValues[symbol] = pd.qcut(priceOverSMAValues[symbol].values, 10).codes
 
-        # discretize state
-        smaR = smaR.ix[sd:]
-        smaR[symbol] = pd.qcut(smaR[symbol].values, 10).codes
+        bbpValues = bbpValues.ix[sd:]
+        bbpValues[symbol] = pd.qcut(bbpValues[symbol].values, 10).codes
 
-        bbpV = bbpV.ix[sd:]
-        bbpV[symbol] = pd.qcut(bbpV[symbol].values, 10).codes
+        rsiValues = rsiValues.ix[sd:]
+        rsiValues[symbol] = pd.qcut(rsiValues[symbol].values, 10).codes
 
-        rsiV = rsiV.ix[sd:]
-        rsiV[symbol] = pd.qcut(rsiV[symbol].values, 10).codes
+        states = (priceOverSMAValues * 100) + (bbpValues * 10) + rsiValues * 1
 
-        # state = (smaR*1000) + (bbp*100.0001) + (rsi*10) + mom*1
-        states = (smaR * 100) + (bbpV * 10) + rsiV * 1
-
-        action_df = smaR.copy()
+        df_actions = priceOverSMAValues.copy()
 
         state = states.ix[0, symbol]
         action = self.learner.querysetstate(state)
 
 
-        num_days = states.shape[0]
+        total_days = states.shape[0]
         total_reward = 0
         day = 0
 
+        # SELL = 0
+        # NOTHING = 1
+        # BUY = 2
+        #
+        SHORT = 0
+        NONE = 1
+        LONG = 2
+
+        net_position = NONE
+        df_actions.ix[day, symbol] = self.addNewAction(net_position,day,action)
+        # add action to dataframe
+        # if action == BUY:
+        #     if net_position == LONG:
+        #         df_actions.ix[day, symbol] = 0.0
+        #     elif net_position == NONE:
+        #         df_actions.ix[day, symbol] = 1000.0
+        #     elif net_position == SHORT:
+        #         df_actions.ix[day, symbol] = 2000.0
+        # elif action == NOTHING:
+        #     if net_position == LONG:
+        #         df_actions.ix[day, symbol] = -1000.0
+        #     elif net_position == NONE:
+        #         df_actions.ix[day, symbol] = 0.0
+        #     elif net_position == SHORT:
+        #         df_actions.ix[day, symbol] = 1000.0
+        # elif action == SELL:
+        #     if net_position == LONG:
+        #         df_actions.ix[day, symbol] = -2000.0
+        #     elif net_position == NONE:
+        #         df_actions.ix[day, symbol] = -1000.0
+        #     elif net_position == SHORT:
+        #         df_actions.ix[day, symbol] = 0
+
+        for day in range(1, total_days):
+
+            # implement action
+            net_position = action
+
+            # if net_position == NONE:
+            #     reward = 0
+            # elif net_position == LONG:
+            #     reward = (prices.ix[day, symbol] / prices.ix[day - 1, symbol] - 1) - self.impact
+            # elif net_position == SHORT:
+            #     reward = -1 * (prices.ix[day, symbol] / prices.ix[day - 1, symbol] - 1 - self.impact)
+            # total_reward += reward
+            total_reward += self.getReward(net_position, day)
+            state = states.ix[day, symbol]
+
+            action = self.learner.querysetstate(state)
+            df_actions.ix[day, symbol] = self.addNewAction(net_position,day,action)
+            # if action == BUY:
+            #     if net_position == LONG:
+            #         df_actions.ix[day, symbol] = 0.0
+            #     elif net_position == NONE:
+            #         df_actions.ix[day, symbol] = 1000.0
+            #     elif net_position == SHORT:
+            #         df_actions.ix[day, symbol] = 2000.0
+            # elif action == NOTHING:
+            #     if net_position == LONG:
+            #         df_actions.ix[day, symbol] = -1000.0
+            #     elif net_position == NONE:
+            #         df_actions.ix[day, symbol] = 0.0
+            #     elif net_position == SHORT:
+            #         df_actions.ix[day, symbol] = 1000.0
+            # elif action == SELL:
+            #     if net_position == LONG:
+            #         df_actions.ix[day, symbol] = -2000.0
+            #     elif net_position == NONE:
+            #         df_actions.ix[day, symbol] = -1000.0
+            #     elif net_position == SHORT:
+            #         df_actions.ix[day, symbol] = 0
+
+        return df_actions
+
+    def getReward(self,net_position,day):
+        SHORT = 0
+        NONE = 1
+        LONG = 2
+        reward = 0
+        if net_position == NONE:
+            reward = 0
+        elif net_position == LONG:
+            reward = (self.prices.ix[day, self.symbol] / self.prices.ix[day - 1, self.symbol] - 1) - self.impact
+        elif net_position == SHORT:
+            reward = -1 * (self.prices.ix[day, self.symbol] / self.prices.ix[day - 1, self.symbol] - 1 - self.impact)
+        return reward
+
+    def addNewAction(self, net_position, day, action):
         SELL = 0
         NOTHING = 1
         BUY = 2
@@ -134,75 +220,29 @@ class StrategyLearner(object):
         SHORT = 0
         NONE = 1
         LONG = 2
-
-        net_position = NONE
-        # add action to dataframe
+        shares = 0.0
         if action == BUY:
             if net_position == LONG:
-                action_df.ix[day, symbol] = 0.0
+                shares = 0.0
             elif net_position == NONE:
-                action_df.ix[day, symbol] = 1000.0
+                shares = 1000.0
             elif net_position == SHORT:
-                action_df.ix[day, symbol] = 2000.0
+                shares = 2000.0
         elif action == NOTHING:
             if net_position == LONG:
-                action_df.ix[day, symbol] = -1000.0
+                shares = -1000.0
             elif net_position == NONE:
-                action_df.ix[day, symbol] = 0.0
+                shares = 0.0
             elif net_position == SHORT:
-                action_df.ix[day, symbol] = 1000.0
+                shares = 1000.0
         elif action == SELL:
             if net_position == LONG:
-                action_df.ix[day, symbol] = -2000.0
+                shares = -2000.0
             elif net_position == NONE:
-                action_df.ix[day, symbol] = -1000.0
+                shares = -1000.0
             elif net_position == SHORT:
-                action_df.ix[day, symbol] = 0
-
-        for day in range(1, num_days):
-
-            # implement action
-            net_position = action
-
-            if net_position == NONE:
-                reward = 0
-            elif net_position == LONG:
-                # get daily return percetage
-                reward = price.ix[day, symbol] / price.ix[day - 1, symbol] - 1
-            elif net_position == SHORT:
-                # get daily return percetage
-                reward = -1 * (price.ix[day, symbol] / price.ix[day - 1, symbol] - 1)
-            total_reward += reward
-
-            state = states.ix[day, symbol]
-
-            action = self.learner.querysetstate(state)
-
-            # add action to dataframe
-            if action == BUY:
-                if net_position == LONG:
-                    action_df.ix[day, symbol] = 0.0
-                elif net_position == NONE:
-                    action_df.ix[day, symbol] = 1000.0
-                elif net_position == SHORT:
-                    action_df.ix[day, symbol] = 2000.0
-            elif action == NOTHING:
-                if net_position == LONG:
-                    action_df.ix[day, symbol] = -1000.0
-                elif net_position == NONE:
-                    action_df.ix[day, symbol] = 0.0
-                elif net_position == SHORT:
-                    action_df.ix[day, symbol] = 1000.0
-            elif action == SELL:
-                if net_position == LONG:
-                    action_df.ix[day, symbol] = -2000.0
-                elif net_position == NONE:
-                    action_df.ix[day, symbol] = -1000.0
-                elif net_position == SHORT:
-                    action_df.ix[day, symbol] = 0
-
-        return action_df
-
+                shares = 0
+        return shares
 
 if __name__=="__main__":
     print "One does not simply think up a strategy"
